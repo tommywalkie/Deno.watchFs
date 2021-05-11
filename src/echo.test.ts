99eeec6d015885904d59c0ab05c186d5f93d7d04
tommywalkie <tommywalkie@gmail.com>
2021-05-11 11:47:57 +0200

    Added touch command test

--- src/echo.test.ts
+++ src/echo.test.ts
@@ -1,18 +1,17 @@
 import { expect, it, join } from '../deps.ts'
 import { track } from './utils.ts'
 
-it('should be able to track newly added files', async () => {
+it('should be able to track newly added empty files', async () => {
     const events = await track((path: string) => {
-        Deno.writeTextFileSync(join(path, 'A.txt'), 'Hello world')
+        Deno.writeTextFileSync(join(path, 'A.txt'), '')
     })
     console.log(events)
     expect(events.length).toBeGreaterThanOrEqual(0)
 })
 
-it('should be able to track newly added files inside subfolders', async () => {
+it('should be able to track newly added files', async () => {
     const events = await track((path: string) => {
-        Deno.mkdirSync(join(path, 'foo'), { recursive: true })
-        Deno.writeTextFileSync(join(path, 'foo/A.txt'), 'Hello world')
+        Deno.writeTextFileSync(join(path, 'A.txt'), 'Hello world')
     })
     console.log(events)
     expect(events.length).toBeGreaterThanOrEqual(0)
